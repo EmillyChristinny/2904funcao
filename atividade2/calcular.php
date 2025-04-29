cab23b885e173c7353415edf376761a06b672953
EmillyChristinny <emillychristinnyal@gmail.com>
2025-04-29 11:16:23 -0300

    atividade2 - Emilly

---
+++ atividade2/calcular.php
@@ -0,0 +1,15 @@
+<?php
+
+$valor1 = $_POST['valor1'];
+function pi($a) {
+    return ($a)%2 ;
+}
+if (return == 1)
+{
+   echo " $a é impar"
+}else {
+echo " $a é par"
+}
+
+
+?>
---
+++ atividade2/index.php
@@ -0,0 +1,4 @@
+<form action="calcular.php" method="post">
+    Valor 1: <input type="number" name="valor1" required><br>
+    <input type="submit" value="Calcular produto">
+</form>
