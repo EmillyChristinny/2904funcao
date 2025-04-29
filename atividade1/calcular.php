910b3053ac5b6d04235d3829f04e34734ea8507c
EmillyChristinny <emillychristinnyal@gmail.com>
2025-04-29 11:10:17 -0300

    atividade 1 - Emilly

---
+++ atividade1/calcular.php
@@ -0,0 +1,11 @@
+<?php
+function multiplicar($a, $b) {
+    return ($a * $b) ;
+}
+
+$valor1 = $_POST['valor1'];
+$valor2 = $_POST['valor2'];
+$m = multiplicar($valor1, $valor2);
+
+echo "O produto do $valor1 e $valor2 é: $m";
+?>
